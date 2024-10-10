3abd708be6299d9fbc29c1397096449d86ebd462
Леонид Мартов <leonidmartov@MacBook-Air-Leonid.local>
2024-10-10 22:20:59 +0300

    tasks done

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,25 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.sql.Time;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "dog")
+    @Scope("prototype")
+    public Dog createDog() {
+        return new Dog();
+    }
+    @Bean(name="timer")
+    @Scope("prototype")
+    public Timer createTime() {
+        return new Timer();
+    }
 }
